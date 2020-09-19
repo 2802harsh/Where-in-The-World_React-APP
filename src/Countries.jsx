ed324109bff8efd47c9d18bec278ac819c2e2446
Harshdeep Singh Pruthi <58396239+2802harsh@users.noreply.github.com>
2020-09-20 03:55:32 +0530

    Remove unnecessary Fade occurence

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Fade from "react-reveal/Fade";
 import Country from "./Country";
 
 function Countries(props) {
